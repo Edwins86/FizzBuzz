dfe201ddf70df73350494c1c282d044e31d2f668
Kamil Bein <spell86.kamil@gmail.com>
2020-12-28 19:24:25 +0100

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public class FizzBuzz 
+    {
+
+        private const string _message1 = "FizzBuzz!";
+        private const string _message2 = "Fizz!";
+        private const string _message3 = "Buzz!";
+        private string _result;
+        public int UserNumber { get; set; }
+        
+
+
+        public string ShowNumber (int userNumber)
+        {
+            if (userNumber %3 == 0 && userNumber %5 == 0)
+            {
+                _result = _message1;
+            }
+            else if (userNumber %3 == 0)
+            {
+                _result = _message2;
+            }
+            else if (userNumber %5 == 0)
+            {
+                _result = _message3;
+            }
+
+            else
+            {
+                _result = userNumber.ToString();
+            }
+
+            return _result;              
+        }
+
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+       
+                        
+        static void Main(string[] args)
+        {
+
+            FizzBuzz fizz = new FizzBuzz();
+
+            
+
+            Console.WriteLine("Witaj graczu, wprowadź swoją cyfrę : ");
+
+            try
+            {
+                fizz.UserNumber  = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Podano niewłaściwą wartość! Spróbuj ponownie");
+            }
+            
+            
+            
+            string result = fizz.ShowNumber(fizz.UserNumber);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+        }
+
+        
+    }
+}
